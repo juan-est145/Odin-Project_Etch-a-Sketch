37136e79bd957ec6cfc9eeae566aada56df180d5
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-03-16 21:16:59 +0100

    First js lines

---
+++ script.js
@@ -0,0 +1,8 @@
+function CreateGrid(size) 
+{
+    const container = document.querySelector(".container");
+    for (let index = 0; index < size; index++) 
+    {
+        const column = document.createElement("div");
+    }    
+}
