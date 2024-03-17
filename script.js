686d5376158df9555f2d20c3ab99211461479902
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-03-17 11:49:12 +0100

    Create grid function is done

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+CreateGrid(16);
+
+
 function CreateGrid(size) 
 {
     const container = document.querySelector(".container");
     for (let index = 0; index < size; index++) 
     {
         const column = document.createElement("div");
+        column.setAttribute("class", "column");
+        for (let index = 0; index < size; index++)
+        {
+            const cell = document.createElement("div");
+            cell.setAttribute("class", "cell");
+            column.appendChild(cell);
+        }
+        container.appendChild(column);
     }    
 }
