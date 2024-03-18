ce5cde1b940fab84d6483a3c755625572777fc1e
Juan Jose Estrella Ramirez <128648875+juan-est145@users.noreply.github.com>
2024-03-18 17:02:03 +0100

    Erased unneccessary curly braces

--- script.js
+++ script.js
@@ -26,13 +26,11 @@ document.querySelector(".container").addEventListener("mouseover", (e)=>
 
 function create_random_color()
 {
-    let arrayOfColors = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+    const arrayOfColors = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
 
     let color_string = "";
 
     for(let i = 0; i < 6; i++)
-    {
         color_string += arrayOfColors[Math.floor(Math.random()* 15)];
-    }
     return `#${color_string}`;
 }
